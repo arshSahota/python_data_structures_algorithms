d2ca6eb409051e9189ca1832a741941e6fe35bf0
Arshdeep Kaur <arshdeep_kaur@bcit.ca>
2026-07-07 14:34:33 -0700

    added time and space complexity

--- two_sum.py
+++ two_sum.py
@@ -11,3 +11,7 @@ class Solution:
 
 sol = Solution()
 print(sol.twoSum([2,7,11,15], 9))
+
+#time complexity O(n) because you loop through the list once, if nums has n elements, the loop runs n times
+#dictionary lookups and insertion are average-case O(1)
+#therefore n * O(1) = O(n)
